719c55343d63a4550e0e82a68ca9f7ca91ba0daa
fcaioribeiro <kkaaiioo70@gmail.com>
2020-09-15 23:11:28 -0300

    iniciando com env

---
+++ bin/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = process.env.SALT_KEY;
+
+module.exports = {
+    connectionStringBD: process.env.URI_MONGO,
+
+    connectionStringDistanceMatrixKey: 'AIzaSyB8DoPsR9DrY8FSOg2PgS5QL0pESf23C5o',
+    connectionStringUpdateImage: ''
+}
--- bin/server.js
+++ bin/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const http = require('http');
 const debug = require('debug');
 const app = require('../app');
