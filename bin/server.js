5249be5f1850a3184c41ee2b6d51578647f2bcaa
fcaioribeiro <kkaaiioo70@gmail.com>
2020-09-15 23:14:39 -0300

    uppercase

--- bin/server.js
+++ bin/server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const debug = require('debug');
 const app = require('../app');
 
-const port = normalizePort(process.env.port || '3000');
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
